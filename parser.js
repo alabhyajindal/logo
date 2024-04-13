476329f516a53ecfb552b362336304fb24386457
Alabhya Jindal <alabhya10@gmail.com>
2024-04-13 20:27:26 +0530

    nice

--- parser.js
+++ parser.js
@@ -1,53 +1,30 @@
 'use strict'
 
-// The goal is to create a nested structure from this string.
-
-// type token {
-//   name: string
-//   arg: number
-//   commands: token[]
-// }
-
 const dynamic = ['fd', 'bk', 'rt', 'lt', 'pc']
 const fixed = ['cs', 'ct', 'pd', 'pu']
 
-class Parser {
-  constructor(text) {
-    this.text = text.split(' ')
-    this.tokens = []
-    this.index = -1
-    this.trunk = true
-  }
-
-  tokenize() {
-    if (this.text.length == 0) return this.tokens
-    // console.log('this.text', this.text)
-
-    let word = this.text[0]
-    word = word.replace('[', '')
-    word = word.replace(']', '')
-
-    const parent = this.trunk ? this.tokens : this.tokens[this.index].commands
-
-    if (word == 'repeat') {
-      const start = this.text.findIndex((t) => t.includes('['))
-      parent.push({
-        name: word,
-        arg: parseInt(this.text[1]),
-        commands: [],
-      })
-
-      this.index++
-      this.trunk = false
-    }
-
-    if (dynamic.includes(word)) {
-      parent.push({ name: word, arg: parseInt(this.text[1]) })
-    } else if (fixed.includes(word)) {
-      parent.push({ name: word })
-    }
-
-    this.text.shift()
-    return this.tokenize()
-  }
+if (dynamic.includes(word)) {
+  parent.push({ name: word, arg: parseInt(this.text[1]) })
+} else if (fixed.includes(word)) {
+  parent.push({ name: word })
 }
+
+function parseExpression(words, result) {
+  for (let [i, word] of words.entries()) {
+    if (word == 'repeat') {
+      result.push({ name: word, arg: words[i + 1] })
+    }
+  }
+  return result
+}
+
+function parse(program) {
+  console.log(program)
+
+  const words = program.split(' ')
+  const result = parseExpression(words, [])
+  console.log(result)
+}
+
+const program = 'repeat 4 [fd 10 rt 90] fd 10'
+parse(program)
